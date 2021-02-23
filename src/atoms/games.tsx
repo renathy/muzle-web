321b8ee760de38357a0f885720ed2d86d9c24f5d
Robert Jin <todo1017@outlook.com>
2021-02-23 09:32:08 +0800

    background, category, game, image; atom

---
+++ src/atoms/games.tsx
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+
+export interface Game {
+  id: number;
+  name: string;
+  description: string;
+}
+
+interface GameState {
+  init: boolean;
+  games: Game[];
+}
+
+const gameAtom = atom({
+  key: 'categoryState',
+  default: {
+    init: false,
+    games: []
+  } as GameState,
+});
+
+export default gameAtom;
